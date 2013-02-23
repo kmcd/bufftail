53e3769c077bcad802a18ae336bd3f460fb9d630
Keith McDonnell <keith@dancingtext.com>
2013-02-23 18:01:03 +0000

    Control chart OK

--- control_chart.rb
+++ control_chart.rb
@@ -20,8 +20,33 @@ trades = [-54.3, 33.2,-66.8,33.2,83.2,33.2,33.2,45.7,33.2,33.2,33.2,-141.8,33.2,
 33.2,33.2,33.2,-4.3,33.2,33.2,-4.3,33.2,-104.3,-29.3,33.2,33.18,33.2,-4.3,33.2,
 -16.8,33.2,33.2,33.2,33.2,-104.3,33.2,33.18,8.2,33.2,-4.3]
 
-g = Gruff::Line.new 500
-g.title = strategy
-g.data :trades, trades
+rolling_mean = trades.each_with_index.map do |t,i| 
+  roll = i < 10 ? i : 10 
+  trades[i-roll..i].to_scale.mean.to_i
+end
 
-g.write "./tmp/line_transparent.png"
+rolling_stdev = trades.each_with_index.map do |t,i| 
+  roll = i < 10 ? i : 10
+  trades[i-roll..i].to_scale.sd
+end[1..-1].unshift 0
+
+ratio = rolling_mean.each_with_index.map do |mean,i|
+  next if rolling_mean[i] == 0 || rolling_stdev[i] == 0
+  rolling_mean[i].to_f / rolling_stdev[i].to_f
+end.flatten.unshift 0
+
+g = Gruff::Line.new
+g.theme = { font_color:'black', background_colors:'white' }
+g.title = strategy
+g.hide_legend = true
+g.line_width = 2
+g.dot_radius = 0
+
+g.data 'Trades', trades, 'grey'
+g.data 'Rolling mean', rolling_mean, 'black'
+g.data 'Mean', [benchmark_mean] * trades.size, 'green'
+g.data 'Zero', [0] * trades.size, 'blue'
+g.data 'Stdev -1', [-benchmark_stdev] * trades.size, 'pink'
+g.data 'Stdev +2', [-benchmark_stdev*2] * trades.size, 'red'
+
+g.write "./tmp/#{strategy.downcase}.png"
