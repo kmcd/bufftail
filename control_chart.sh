ca3e92626dbfe6d9f41272ad5762ced924cb5b3c
Keith McDonnell <keith@dancingtext.com>
2013-02-26 17:33:21 +0000

    Automate control chart generation

---
+++ control_chart.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ruby control_chart.rb ED_RT_CTL
+sleep 2
+ruby control_chart.rb EI_RTL
+sleep 2
+ruby control_chart.rb EI_RT_CTL
+sleep 2
+ruby control_chart.rb SS_RTL  
+sleep 2
+ruby control_chart.rb SS_RT_CTL
+sleep 2
+./control_chart.rhtml
+open ./tmp/control_charts.html
