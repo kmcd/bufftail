b70398b76a84cb211ae2c977b666569a0a71e1c7
Keith McDonnell <keith@dancingtext.com>
2013-02-26 15:50:43 +0000

    Ensure latest quotes available before generating entry signals

--- signal_scan.rb
+++ signal_scan.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
 # N.B. Ensure Amibroker is running
-
 unless signal = ARGV.first
   puts "USAGE: ./signal_scan.rb ED_RT_CTL"
   puts "Must be one of:"
@@ -12,6 +11,14 @@ end
 require 'win32ole'
 @amibroker = WIN32OLE.new 'Broker.Application'
 @amibroker.LoadDatabase "C:\\\\Program\ Files\\Amibroker\\IQ_eod"
+
+def ensure_latest_quotes_available
+  filename = "C:\\\\Program\ Files\\Amibroker\\Formulas\\Custom\\backfill.apx"
+  puts "= Backfilling"
+  wfa = @amibroker.AnalysisDocs.Open filename
+  wfa.Run 0
+  sleep 1 while wfa.IsBusy
+end
 
 def export_signals(basename)
   filename = "C:\\\\Program\ Files\\Amibroker\\Formulas\\Signals\\#{basename}.apx"
@@ -26,4 +33,5 @@ def export_signals(basename)
   wfa.Close
 end
 
+ensure_latest_quotes_available
 export_signals signal
