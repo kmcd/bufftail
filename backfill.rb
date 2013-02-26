5543dad5c7d586fb11f89520cd324d6e06311ecf
Keith McDonnell <keith@dancingtext.com>
2013-02-26 17:40:01 +0000

    Backfill once before market close

---
+++ backfill.rb
@@ -0,0 +1,9 @@
+require 'win32ole'
+amibroker = WIN32OLE.new 'Broker.Application'
+amibroker.LoadDatabase "C:\\\\Program\ Files\\Amibroker\\IQ_eod"
+
+filename = "C:\\\\Program\ Files\\Amibroker\\Formulas\\Custom\\backfill.apx"
+puts "= Backfilling"
+wfa = amibroker.AnalysisDocs.Open filename
+wfa.Run 0
+sleep 1 while wfa.IsBusy
