6291332eba234d9d1f9d9a9553ef99a5d6716ce3
Keith McDonnell <keith@dancingtext.com>
2013-02-27 17:27:22 +0000

    ED ZS L -> ok

--- control_chart.sh
+++ control_chart.sh
@@ -5,9 +5,13 @@ ruby control_chart.rb EI_RTL
 sleep 2
 ruby control_chart.rb EI_RT_CTL
 sleep 2
-ruby control_chart.rb SS_RTL  
+ruby control_chart.rb SS_RTL
 sleep 2
 ruby control_chart.rb SS_RT_CTL
+sleep 2
+ruby control_chart.rb SB_RT_CTS
+sleep 2
+ruby control_chart.rb ED_ZS_L
 sleep 2
 ./control_chart.rhtml
 open ./tmp/control_charts.html
