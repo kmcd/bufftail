6d09538283e79762807ef8bf5f52d5b62ae341db
Keith McDonnell <keith@dancingtext.com>
2013-02-28 19:04:49 +0000

    Remove old export file: should be no more stale signals now

--- signal_scan.rb
+++ signal_scan.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 # N.B. Ensure Amibroker is running
+require 'win32ole'
+require 'fileutils'
+
 if ARGV.empty?
   puts "USAGE: ./signal_scan.rb ED_RT_CTL [optional strat 2] ..."
   puts "Must be one of:"
@@ -8,7 +11,6 @@ if ARGV.empty?
   exit
 end
 
-require 'win32ole'
 @amibroker = WIN32OLE.new 'Broker.Application'
 
 def export_signals(basename)
@@ -18,8 +20,10 @@ def export_signals(basename)
   wfa.Run 1
   sleep 1 while wfa.IsBusy
   
-  export_file = File.basename(filename).gsub /apx/, 'csv'
-  wfa.export "C:\\\\tmp\\#{export_file}"
+  export_file = "C:\\\\tmp\\#{File.basename(filename).gsub(/apx/, 'csv')}"
+  FileUtils.rm export_file if File.exist? export_file
+  
+  wfa.export export_file
   puts "= Exported     C:\\\\tmp\\#{export_file}"
   wfa.Close
 end
