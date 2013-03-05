1c9f8e087eadff3eea525b48976107ab070fc401
Keith McDonnell <keith@dancingtext.com>
2013-03-05 19:18:12 +0000

    Position sizing for live strats

--- live_trade.sh
+++ live_trade.sh
@@ -1,13 +1,12 @@
 # LIFFE: Short Sterling
-ruby place_orders.rb SS_RTL 0.07
+ruby place_orders.rb SS_RTL 0.08
 
 # LIFFE: Euribor
-ruby place_orders.rb EI_RTL 0.03
-ruby place_orders.rb EI_RT_CTL 0.08
+ruby place_orders.rb EI_RT_CTL 0.04
 
-# GLOBEX order will be 60 mins. early
-ruby place_orders.rb SB_RT_CTS 0.03 # 0.11 verify paper trade before increacing
+# EUREX: Schatz, GLOBEX: 2 year note (order 1 hr. early if placed simulataneously)
+ruby place_orders.rb SB_RT_CTS 0.13
 
 # GLOBEX: Eurodollar
 ruby place_orders.rb ED_RT_CTL 0.03
-ruby place_orders.rb ED_ZS_L 0.14
+ruby place_orders.rb ED_ZS_L 0.17
